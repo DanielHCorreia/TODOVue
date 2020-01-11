1629500c539ab18df2e2d14e0bafb0a8e1619aef
DanielCorreia <dhac@cin.ufpe.br>
2020-01-11 10:54:41 -0300

    Revert "- Configurado Ionic no projeto."
    This reverts commit 09822026a03d15a300effb9a53f5d352b1c55359.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
-import Ionic from '@ionic/vue';
-import '@ionic/core/css/ionic.bundle.css';
 
-Vue.use(Ionic);
 Vue.config.productionTip = false
 
 new Vue({
